e0e22a96bc4506fe768606cfb9e4dca0ab058b5f
Admin <admin.push@hackerrank.workspace>
2024-03-01 08:53:11 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+const stack =[];
+
+const maxStackSize = 5;
+
+const inputBox = document.getElementById("input");
+const pushButton = document.getElementById("pushButton");
+const emptyButton = document.getElementById("emptyButton");
+const peekButton = document.getElementById("peekButton");
+
+
+
+pushButton.addEventListener('click', function(){
+  const value = inputBox.value.trim();
+  if(value == ''){
+    alert("Please enter a value!")
+  }
+  else if(stack.length == maxStackSize){
+    alert("Stack was already full!")
+  }
+  else{
+    stack.push(value);
+    updateStackDisplay()
+  }
+})
+
+emptyButton.addEventListener('click', function(){
+  if(stack.length == 0){
+    alert("Yes, Stack is empty!")
+  }
+  else{
+    alert("Yes, Stack is not empty!")
+  }
+})
+
+peekButton.addEventListener('click', function(){
+  if(stack.length == 0){
+    alert("Operation not allowed")
+  }
+  else{
+    alert("Top item:"+ stack[stack.length-1]);
+  }
+})
+
+
+function updateStackDisplay(){
+  for(let i=0; i<maxStackSize; i++){
+    const stackItem = document.getElementById((i+1));
+    if(i < stack.length){
+      stackItem.textContent = stack[i];
+    }
+    else{
+      stackItem.textContent = '';
+    }
+  }
+}
